c5f4fe9ff915086818425f5553cfd2c797bee5d9
Zeno- <crobbins@localhost.localdomain>
2013-11-16 18:51:04 +1000

    Broke some logic up into inline functions

--- rand/randmt.c
+++ rand/randmt.c
@@ -24,24 +24,24 @@
 
 /* See Knuth, TAOCP Vol 2, pp. 106-107 (line 13 of table)
  */
-#define KNUTH_MULTIPLIER    1812433253
+#define KNUTH_MULTIPLIER    1812433253UL
 #define KNUTH_SHIFT         30
 
 #define MT_UTNLEN           624
 #define MT_MAGICN           397
-#define MT_MATRIX           0x9908b0df
+#define MT_MATRIX           0x9908b0dfUL
 
-#define MT_SHIFTB           11
-#define MT_SHIFTC           7
-#define MT_SHIFTD           15
-#define MT_SHIFTE           18
+#define MT_SHIFTA           11
+#define MT_SHIFTB           7
+#define MT_SHIFTC           15
+#define MT_SHIFTD           18
 
-#define MT_MAGICMASKA       0x9d2c5680
-#define MT_MAGICMASKB       0xefc60000
+#define MT_MAGICMASKA       0x9d2c5680UL
+#define MT_MAGICMASKB       0xefc60000UL
 
-#define MT_BIT31            0x80000000
-#define MT_BITS0TO30        0x7FFFFFFF
-#define MT_MASK32           0xFFFFFFFF
+#define MT_BIT31            0x80000000UL
+#define MT_BITS0TO30        0x7FFFFFFFUL
+#define MT_MASK32           0xFFFFFFFFUL
 
 /* The question remains whether or not using unsinged long is faster than
  * using uint32_t. If, for example, a 64-bit machine has to shift unaligned
@@ -62,6 +62,14 @@ struct mt {
 
 inline static void mt_gen_(struct mt *mt);
 
+inline static unsigned long 
+knuth_prng(unsigned long pn, unsigned long n)
+{
+    unsigned nrn = KNUTH_MULTIPLIER * (pn ^ (pn >> KNUTH_SHIFT)) + n;
+    MASKRESULT(nrn, nrn);
+    return nrn;
+}
+
 inline static void 
 mt_init_(struct mt *mt, unsigned long seed)
 {
@@ -69,15 +77,26 @@ mt_init_(struct mt *mt, unsigned long seed)
     unsigned long *utn = mt->utn;
     
     utn[0] = seed & MT_MASK32;
-    for (i = 1; i < MT_UTNLEN; i++) {
-        utn[i] = KNUTH_MULTIPLIER  * (utn[i-1] ^ (utn[i-1] >> KNUTH_SHIFT)) + i;
-        MASKRESULT(utn[i], utn[i]);
-    }
+    for (i = 1; i < MT_UTNLEN; i++)
+        utn[i] = knuth_prng(utn[i-1], i);
     
-    /* Set to i (MT_UTNLEN) so that first call to mt_rand_() triggers call
-     * to mt_gen_()
+    /* Set mt->idx to i (MT_UTNLEN) so that the first call to mt_rand_()
+     * triggers a call to mt_gen_()
      */
     mt->idx = i;
+}
+
+inline static unsigned long
+mt_combinebits_(unsigned long a, unsigned long b)
+{
+    return (a & MT_BIT31) | (b & MT_BITS0TO30);
+}
+
+inline static unsigned long
+mt_matrixmultiply_(unsigned long a, unsigned long b)
+{
+    static unsigned long magic[2] = {0UL, MT_MATRIX};
+    return a ^ (b >> 1) ^ magic[b & 0x01];
 }
 
 inline static void 
@@ -85,8 +104,7 @@ mt_gen_(struct mt *mt)
 {
     int i;
     unsigned long *utn = mt->utn, y;    
-    static unsigned long magic[2] = {0UL, MT_MATRIX};
-
+    
     /* Note: The "reference algorithm" checks if mt_init_() has been called and
      *       if not calls mt_init_() with a seed of 5489UL. Because of the
      *       way this implementation is designed it's not possible for 
@@ -95,16 +113,16 @@ mt_gen_(struct mt *mt)
      */
         
     for (i = 0; i < MT_UTNLEN - MT_MAGICN; i++) {
-        y = (utn[i] & MT_BIT31) | (utn[i + 1] & MT_BITS0TO30);
-        utn[i] = utn[i + MT_MAGICN] ^ (y >> 1) ^ magic[y & 0x1];
+        y = mt_combinebits_(utn[i], utn[i + 1]);
+        utn[i] = mt_matrixmultiply_(utn[i + MT_MAGICN], y);
     }
     for (; i < MT_UTNLEN - 1; i++) {
-        y = (utn[i] & MT_BIT31) | (utn[i + 1] & MT_BITS0TO30);
-        utn[i] = utn[i + MT_MAGICN - MT_UTNLEN] ^ (y >> 1) ^ magic[y & 0x1];
+        y = mt_combinebits_(utn[i], utn[i + 1]);
+        utn[i] = mt_matrixmultiply_(utn[i + MT_MAGICN - MT_UTNLEN], y);
     }
-    y = (utn[i] & MT_BIT31) | (utn[0] & MT_BITS0TO30);
-    utn[i] = utn[MT_MAGICN - 1] ^ (y >> 1) ^ magic[y & 0x1];
-        
+    y = mt_combinebits_(utn[i], utn[0]);
+    utn[i] = mt_matrixmultiply_(utn[MT_MAGICN - 1], y);
+            
     mt->idx = 0;
 }
 
@@ -117,10 +135,10 @@ mt_rand_(struct mt *mt)
         mt_gen_(mt);
     
     y = mt->utn[mt->idx++];
-    y ^= (y >> MT_SHIFTB);
-    y ^= (y << MT_SHIFTC) & MT_MAGICMASKA;
-    y ^= (y << MT_SHIFTD) & MT_MAGICMASKB;
-    y ^= (y >> MT_SHIFTE);
+    y ^= (y >> MT_SHIFTA);
+    y ^= (y << MT_SHIFTB) & MT_MAGICMASKA;
+    y ^= (y << MT_SHIFTC) & MT_MAGICMASKB;
+    y ^= (y >> MT_SHIFTD);
     
     return y;
 }
